2b4a118717eaa2066ad2a656d62e1c2e4d5ee976
Jordan So <jso01@student.ubc.ca>
2024-02-07 17:51:27 -0800

    Implement expandable for projects

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function addButtonListeners() {
+    document.querySelectorAll(".project-expand-button").forEach(button => {
+        button.addEventListener("click", ()=> {
+            let expandable = button.nextElementSibling;
+            if (expandable.style.maxHeight) {
+                expandable.style.maxHeight = null;
+            } else {
+                expandable.style.maxHeight = expandable.scrollHeight + 'px';
+            }
+            console.log(button, expandable);
+        })
+        
+    });
+}
+
+addButtonListeners();
