c29978f97aafd9597fadd4393664970181e813e4
Jordnos <jordy7789@hotmail.com>
2024-02-10 17:12:28 -0800

    Change arrow direction on project expand

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,16 @@ function addButtonListeners() {
     document.querySelectorAll(".project-expand-button").forEach(button => {
         button.addEventListener("click", ()=> {
             let expandable = button.nextElementSibling;
+            let svg = button.querySelector("svg");
+
             if (expandable.style.maxHeight) {
                 expandable.style.maxHeight = null;
+                svg.setAttribute('transform', 'scale(-1, 1)');
             } else {
                 expandable.style.maxHeight = expandable.scrollHeight + 'px';
+                svg.setAttribute('transform', 'scale(1, -1)');
             }
         })
-        
     });
 }
 
